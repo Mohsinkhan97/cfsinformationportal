117adf3f33fed86ef45df34526b0cd3eb4c4cd9a
Mohsinkhan97 <mohsinssg1@gmail.com>
2025-02-03 03:06:37 +0500

    all recent changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Optional: Enables React Strict Mode
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true, // Optional: Enables React Strict Mode
+};
+
+module.exports = nextConfig;
