b3852dafa40004504a7e28e1b99e8bef7a0f66d7
Mohsinkhan97 <mohsinssg1@gmail.com>
2025-02-03 02:28:58 +0500

    next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Optional: Enables React Strict Mode
+  };
+  
+  export default nextConfig;
+  
